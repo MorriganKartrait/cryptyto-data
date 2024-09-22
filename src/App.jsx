835adff41c85ab6a621e81f30af1b80898d02cbf
SawyerSheen <georgijmozeew@gmail.com>
2024-09-22 19:50:27 +0300

    added global data component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Navigation from "./Navigation";
 import ListCoins from "./ListCoins";
 import CoinPriceSection from "./CoinPriceSection";
 import CoinPage from "./CoinPage";
+import GlobalData from "./GlobalDAta";
 
 function App() {
   return (
     <Container>
       <Navigation />
+      <GlobalData />
       {/* <ListCoins /> */}
       <CoinPage />
     </Container>
